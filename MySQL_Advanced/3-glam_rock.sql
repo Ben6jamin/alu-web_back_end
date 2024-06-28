d476c809d9d60038d9a70a2f3452bccf8d79af6a
Ben6jamin <113389731+Ben6jamin@users.noreply.github.com>
2024-06-28 15:09:01 +0000

    rock

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -2,6 +2,9 @@
 -- Making a query with band_name and lifespan in years.
 SELECT 
     band_name, 
+    formed,
+    split,
+    IF(ISNULL(split), YEAR(CURDATE()), split) AS end_year,
     (IF(ISNULL(split), YEAR(CURDATE()), split) - formed) AS lifespan 
 FROM 
     metal_bands 
